72ce0fc2783d4dc5e3211108f76f82102b7d580b
Adam Kowalski <adam.kowalski.work@gmail.com>
2018-11-17 11:05:33 -0800

    foster station volume table

--- preprocess.py
+++ preprocess.py
@@ -3,16 +3,59 @@ import pandas as pd
 
 
 # %% stations
-stations = pd.read_csv('data/freeway_stations.csv')
-stations = stations[['locationtext', 'length']]
+stations = pd.read_csv(
+    'data/freeway_stations.csv', usecols=['locationtext', 'length'])
+stations.rename(
+    columns={
+        'locationtext': 'station:locationtext',
+        'length': 'station:length'},
+    inplace=True)
 
 # %% detectors
-detectors = pd.read_csv('data/freeway_detectors.csv')
-detectors = detectors[['detectorid', 'locationtext']]
+detectors = pd.read_csv(
+    'data/freeway_detectors.csv', usecols=['detectorid', 'locationtext'])
+detectors.rename(
+    columns={
+        'detectorid': 'detector:id',
+        'locationtext': 'detector:locationtext'},
+    inplace=True)
 
 # %% loop data
-loop_data = pd.read_csv('data/freeway_loopdata_OneHour.csv')
-loop_data = loop_data[['detectorid', 'starttime', 'volume', 'speed']]
+loop_data = pd.read_csv(
+    'data/freeway_loopdata_OneHour.csv',
+    usecols=['detectorid', 'starttime', 'volume', 'speed'])
+loop_data.rename(
+    columns={
+        'detectorid': 'loopdata:detectorid',
+        'starttime': 'loopdata:starttime',
+        'volume': 'loopdata:volume',
+        'speed': 'loopdata:speed'},
+    inplace=True)
+
+
+# %% loop data start time
+loop_data_start_time = loop_data['loopdata:starttime']
+
+
+def loop_data_in_date_range(start_date, end_date):
+    lower_bound = loop_data_start_time >= start_date
+    upper_bound = loop_data_start_time < end_date
+    return loop_data[lower_bound & upper_bound]
+
+
+# %% foster station volume september 21, TODO: change dates
+filtered_loop_data = loop_data_in_date_range(
+    '9/15/2011 0:00:00', '9/15/2011 0:10:00')
+loop_data_volume = filtered_loop_data['loopdata:volume'].dropna().values.sum()
+
+foster_station_volume = pd.DataFrame(
+    [loop_data_volume], columns=['fosterstation:volume'])
+
+
+# %% foster station travel times september 22, TODO: change dates
+filtered_loop_data = loop_data_in_date_range(
+    '9/15/2011 0:10:00', '9/15/2011 0:20:00')
+filtered_loop_data['loopdata:speed'].dropna().values
 
 # %% visualize
 stations.head()
@@ -20,3 +63,6 @@ stations.head()
 detectors.head()
 
 loop_data.head()
+
+
+foster_station_volume.head()

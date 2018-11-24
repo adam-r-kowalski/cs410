8aae094286b3c0988d5c82524c21ffc1bd98fcb9
Adam Kowalski <adam.kowalski.work@gmail.com>
2018-11-24 12:36:33 -0800

    Finish formatting all data!!!

--- preprocess.py
+++ preprocess.py
@@ -1,7 +1,21 @@
 # %% imports
 import pandas as pd
+import numpy as np
 from datetime import timedelta
 from typing import Mapping
+
+
+# %% write tables for loading
+def write_tables_for_loading(filename):
+    (pd
+        .read_csv(f'data/{filename}.csv')
+        .to_csv(f'tables/{filename}.csv', header=None))
+
+
+write_tables_for_loading('freeway_stations')
+write_tables_for_loading('freeway_detectors')
+write_tables_for_loading('highways')
+write_tables_for_loading('freeway_loopdata')
 
 
 # %% read_csv
@@ -26,9 +40,8 @@ detectors = read_csv(
         'locationtext': 'detector:locationtext'
     })
 
-# TODO: use all loop data instead of just one hour
 loop_data = read_csv(
-    'data/freeway_loopdata_OneHour.csv',
+    'data/freeway_loopdata.csv',
     columns={
         'detectorid': 'loopdata:detectorid',
         'starttime': 'loopdata:starttime',
@@ -67,10 +80,10 @@ def loop_data_in_date_range(start_date: str, end_date: str,
     return loop_data[lower_bound & upper_bound]
 
 
-# %% foster station volume september 21, TODO: change dates
+# %% foster station volume september 21
 def foster_station_volume_september_21():
     filtered_loop_data = loop_data_in_date_range(
-        '9/15/2011 0:00:00', '9/15/2011 0:10:00')
+        '2011-09-21 00:00:00', '2011-09-22 00:00:00')
 
     loop_data_volume = (filtered_loop_data['loopdata:volume']
                         .dropna()
@@ -86,13 +99,13 @@ foster_station_volume = foster_station_volume_september_21()
 # %% add minutes to time
 def add_minutes_to_time(time, minutes=5):
     new_time = pd.to_datetime(time) + timedelta(minutes=minutes)
-    return new_time.strftime("%-m/%d/%Y %-H:%M:%S")
+    return new_time.strftime("%Y-%m-%d %-H:%M:%S")
 
 
 # %% mean travel times between time periods
 def mean_travel_times_between_time_periods(data_frame: pd.DataFrame,
                                            start_time: str, end_time: str,
-                                           interval: int) -> pd.DataFrame:
+                                           interval: int = 5) -> pd.DataFrame:
     filtered_loop_data = loop_data_in_date_range(start_time, end_time)
 
     _, foster_station_length = (stations
@@ -108,11 +121,12 @@ def mean_travel_times_between_time_periods(data_frame: pd.DataFrame,
             start_time, interval_time,
             filtered_loop_data, filtered_loop_data_start_time)
 
-        mean_speed = interval_data['loopdata:speed'].dropna().values.mean()
+        speeds = interval_data['loopdata:speed'].dropna().values
 
-        # TODO: convert mean speed from mph to mps and report travel time in seconds
-        insert_row(data_frame,
-                   [start_time, foster_station_length / mean_speed * 3600])
+        if len(speeds) > 0:
+            mean_speed = speeds.mean()
+            insert_row(data_frame,
+                       [start_time, foster_station_length / mean_speed * 3600])
 
         start_time = interval_time
 
@@ -121,38 +135,36 @@ def mean_travel_times_between_time_periods(data_frame: pd.DataFrame,
     return data_frame
 
 
-# %% foster station travel times september 22, TODO: change dates, refactor code?
+# %% foster station travel times september 22
 def foster_station_travel_times_september_22():
     foster_station_travel_times = pd.DataFrame(
         columns=['fosterstation:starttime', 'fosterstation:traveltime'])
 
     return mean_travel_times_between_time_periods(
         foster_station_travel_times,
-        start_time='9/15/2011 0:10:00',
-        end_time='9/15/2011 0:20:00',
-        interval=1)
+        start_time='2011-09-22 00:00:00',
+        end_time='2011-09-23 00:00:00')
 
 
 foster_station_travel_times = foster_station_travel_times_september_22()
 
 
-# %% foster station peak travel times, TODO: change dates, refactor code?
+# %% foster station peak travel times
 def foster_station_peak_travel_times_september_22() -> pd.DataFrame:
     foster_station_travel_times = pd.DataFrame(
         columns=['fosterstation:peakperiod', 'fosterstation:traveltime'])
 
-    # change time peroids to 7am 9am and interval to 120
     mean_travel_times_between_time_periods(
         foster_station_travel_times,
-        start_time='9/15/2011 0:10:00',
-        end_time='9/15/2011 0:20:00',
-        interval=10)
+        start_time='2011-09-22 07:00:00',
+        end_time='2011-09-22 09:00:00',
+        interval=120)
 
     return mean_travel_times_between_time_periods(
         foster_station_travel_times,
-        start_time='9/15/2011 0:30:00',
-        end_time='9/15/2011 0:40:00',
-        interval=10)
+        start_time='2011-09-22 16:00:00',
+        end_time='2011-09-22 18:00:00',
+        interval=120)
 
 
 peak_travel_times = foster_station_peak_travel_times_september_22()
@@ -166,12 +178,19 @@ detectors.head()
 loop_data.head()
 
 
+high_speed.to_csv('tables/high_speed.csv', header=None)
 high_speed.head()
 
+
+foster_station_volume.to_csv('tables/foster_station_volume.csv', header=None)
 foster_station_volume.head()
 
 
+foster_station_travel_times.to_csv(
+    'tables/foster_station_travel_times.csv', header=None)
 foster_station_travel_times.head()
 
 
+peak_travel_times.to_csv(
+    'tables/foster_station_peak_travel_times.csv', header=None)
 peak_travel_times.head()

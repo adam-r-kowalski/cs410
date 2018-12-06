dd524d153c7d226cd4e6cb62a3892428e890d6b1
Sohan Tamang <tamangsohan@gmail.com>
2018-12-06 09:30:43 -0800

    Added scripts to import tables

---
+++ scripts/import_loopdata.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+./hbase org.apache.hadoop.hbase.mapreduce.ImportTsv -Dimporttsv.separator=',' -Dimporttsv.columns=HBASE_ROW_KEY,freewayloopdata:did,freewayloopdata:starttime,freewayloopdata:volume,freewayloopdata:speed,freewayloopdata:occupancy,freewayloopdata:status,freewayloopdata:flags freewayloopdata tables/loopdata/freeway_loopdata00.csv
+
+
+
+./hbase org.apache.hadoop.hbase.mapreduce.ImportTsv -Dimporttsv.separator=',' -Dimporttsv.columns=HBASE_ROW_KEY,freewayloopdata:did,freewayloopdata:starttime,freewayloopdata:volume,freewayloopdata:speed,freewayloopdata:occupancy,freewayloopdata:status,freewayloopdata:flags freewayloopdata tables/loopdata/freeway_loopdata01.csv
+
+
+
+
+./hbase org.apache.hadoop.hbase.mapreduce.ImportTsv -Dimporttsv.separator=',' -Dimporttsv.columns=HBASE_ROW_KEY,freewayloopdata:did,freewayloopdata:starttime,freewayloopdata:volume,freewayloopdata:speed,freewayloopdata:occupancy,freewayloopdata:status,freewayloopdata:flags freewayloopdata tables/loopdata/freeway_loopdata02.csv
+
+
+
+./hbase org.apache.hadoop.hbase.mapreduce.ImportTsv -Dimporttsv.separator=',' -Dimporttsv.columns=HBASE_ROW_KEY,freewayloopdata:did,freewayloopdata:starttime,freewayloopdata:volume,freewayloopdata:speed,freewayloopdata:occupancy,freewayloopdata:status,freewayloopdata:flags freewayloopdata tables/loopdata/freeway_loopdata03.csv
+
+
+
+./hbase org.apache.hadoop.hbase.mapreduce.ImportTsv -Dimporttsv.separator=',' -Dimporttsv.columns=HBASE_ROW_KEY,freewayloopdata:did,freewayloopdata:starttime,freewayloopdata:volume,freewayloopdata:speed,freewayloopdata:occupancy,freewayloopdata:status,freewayloopdata:flags freewayloopdata tables/loopdata/freeway_loopdata04.csv
+
+
+
+
+./hbase org.apache.hadoop.hbase.mapreduce.ImportTsv -Dimporttsv.separator=',' -Dimporttsv.columns=HBASE_ROW_KEY,freewayloopdata:did,freewayloopdata:starttime,freewayloopdata:volume,freewayloopdata:speed,freewayloopdata:occupancy,freewayloopdata:status,freewayloopdata:flags freewayloopdata tables/loopdata/freeway_loopdata05.csv
+
+
+
+./hbase org.apache.hadoop.hbase.mapreduce.ImportTsv -Dimporttsv.separator=',' -Dimporttsv.columns=HBASE_ROW_KEY,freewayloopdata:did,freewayloopdata:starttime,freewayloopdata:volume,freewayloopdata:speed,freewayloopdata:occupancy,freewayloopdata:status,freewayloopdata:flags freewayloopdata tables/loopdata/freeway_loopdata06.csv
+
+
+
+./hbase org.apache.hadoop.hbase.mapreduce.ImportTsv -Dimporttsv.separator=',' -Dimporttsv.columns=HBASE_ROW_KEY,freewayloopdata:did,freewayloopdata:starttime,freewayloopdata:volume,freewayloopdata:speed,freewayloopdata:occupancy,freewayloopdata:status,freewayloopdata:flags freewayloopdata tables/loopdata/freeway_loopdata07.csv
+
+
+
+
+./hbase org.apache.hadoop.hbase.mapreduce.ImportTsv -Dimporttsv.separator=',' -Dimporttsv.columns=HBASE_ROW_KEY,freewayloopdata:did,freewayloopdata:starttime,freewayloopdata:volume,freewayloopdata:speed,freewayloopdata:occupancy,freewayloopdata:status,freewayloopdata:flags freewayloopdata tables/loopdata/freeway_loopdata08.csv
+
+
+
+./hbase org.apache.hadoop.hbase.mapreduce.ImportTsv -Dimporttsv.separator=',' -Dimporttsv.columns=HBASE_ROW_KEY,freewayloopdata:did,freewayloopdata:starttime,freewayloopdata:volume,freewayloopdata:speed,freewayloopdata:occupancy,freewayloopdata:status,freewayloopdata:flags freewayloopdata tables/loopdata/freeway_loopdata09.csv
+
+
+
+./hbase org.apache.hadoop.hbase.mapreduce.ImportTsv -Dimporttsv.separator=',' -Dimporttsv.columns=HBASE_ROW_KEY,freewayloopdata:did,freewayloopdata:starttime,freewayloopdata:volume,freewayloopdata:speed,freewayloopdata:occupancy,freewayloopdata:status,freewayloopdata:flags freewayloopdata tables/loopdata/freeway_loopdata10.csv
+
+
+
+
+./hbase org.apache.hadoop.hbase.mapreduce.ImportTsv -Dimporttsv.separator=',' -Dimporttsv.columns=HBASE_ROW_KEY,freewayloopdata:did,freewayloopdata:starttime,freewayloopdata:volume,freewayloopdata:speed,freewayloopdata:occupancy,freewayloopdata:status,freewayloopdata:flags freewayloopdata tables/loopdata/freeway_loopdata11.csv
+
+
+
+./hbase org.apache.hadoop.hbase.mapreduce.ImportTsv -Dimporttsv.separator=',' -Dimporttsv.columns=HBASE_ROW_KEY,freewayloopdata:did,freewayloopdata:starttime,freewayloopdata:volume,freewayloopdata:speed,freewayloopdata:occupancy,freewayloopdata:status,freewayloopdata:flags freewayloopdata tables/loopdata/freeway_loopdata12.csv
+
+
+
+./hbase org.apache.hadoop.hbase.mapreduce.ImportTsv -Dimporttsv.separator=',' -Dimporttsv.columns=HBASE_ROW_KEY,freewayloopdata:did,freewayloopdata:starttime,freewayloopdata:volume,freewayloopdata:speed,freewayloopdata:occupancy,freewayloopdata:status,freewayloopdata:flags freewayloopdata tables/loopdata/freeway_loopdata13.csv
+
+
+
+
+./hbase org.apache.hadoop.hbase.mapreduce.ImportTsv -Dimporttsv.separator=',' -Dimporttsv.columns=HBASE_ROW_KEY,freewayloopdata:did,freewayloopdata:starttime,freewayloopdata:volume,freewayloopdata:speed,freewayloopdata:occupancy,freewayloopdata:status,freewayloopdata:flags freewayloopdata tables/loopdata/freeway_loopdata14.csv
+
+
+
+./hbase org.apache.hadoop.hbase.mapreduce.ImportTsv -Dimporttsv.separator=',' -Dimporttsv.columns=HBASE_ROW_KEY,freewayloopdata:did,freewayloopdata:starttime,freewayloopdata:volume,freewayloopdata:speed,freewayloopdata:occupancy,freewayloopdata:status,freewayloopdata:flags freewayloopdata tables/loopdata/freeway_loopdata15.csv
+
+
+
+./hbase org.apache.hadoop.hbase.mapreduce.ImportTsv -Dimporttsv.separator=',' -Dimporttsv.columns=HBASE_ROW_KEY,freewayloopdata:did,freewayloopdata:starttime,freewayloopdata:volume,freewayloopdata:speed,freewayloopdata:occupancy,freewayloopdata:status,freewayloopdata:flags freewayloopdata tables/loopdata/freeway_loopdata16.csv
+
+
+
+
+./hbase org.apache.hadoop.hbase.mapreduce.ImportTsv -Dimporttsv.separator=',' -Dimporttsv.columns=HBASE_ROW_KEY,freewayloopdata:did,freewayloopdata:starttime,freewayloopdata:volume,freewayloopdata:speed,freewayloopdata:occupancy,freewayloopdata:status,freewayloopdata:flags freewayloopdata tables/loopdata/freeway_loopdata17.csv
+
